60eb063648d293f0b2a37d1fb18a63c7891a96bc
rolyPolyVole <yezhanting@gmail.com>
2024-12-28 12:43:38 +0000

    Add world name and toBukkit() method to `EntityLocation`

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/config/structure/ConfigComponents.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/config/structure/ConfigComponents.kt
@@ -1,12 +1,15 @@
 package foundation.esoteric.fireworkwarslobbyplugin.config.structure
 
 data class EntityLocation(
+    val world: String,
     val x: Double,
     val y: Double,
     val z: Double,
     val yaw: Float,
     val pitch: Float
-)
+) {
+    fun toBukkit() = org.bukkit.Location(Bukkit.getWorld(world), x, y, z, yaw, pitch)
+}
 
 data class NPCData(
     val name: String,
