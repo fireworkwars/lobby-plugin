0c5cd89a3e5a48f06c2fac8c28c2b1bcaa524d78
rolyPolyVole <yezhanting@gmail.com>
2024-12-28 12:48:41 +0000

    Add NPC class that creates and controls NPCs

---
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/npc/NPC.kt
@@ -0,0 +1,110 @@
+package foundation.esoteric.fireworkwarslobby.npc
+
+import com.destroystokyo.paper.profile.CraftPlayerProfile
+import com.destroystokyo.paper.profile.ProfileProperty
+import foundation.esoteric.fireworkwarslobby.FireworkWarsLobbyPlugin
+import foundation.esoteric.fireworkwarslobby.config.structure.NPCData
+import foundation.esoteric.fireworkwarslobby.npc.connection.EmptyConnection
+import foundation.esoteric.fireworkwarslobby.util.NMSUtil
+import net.minecraft.commands.arguments.EntityAnchorArgument
+import net.minecraft.core.BlockPos
+import net.minecraft.network.protocol.game.*
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket.Action
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
+import net.minecraft.server.level.ClientInformation
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.network.CommonListenerCookie
+import net.minecraft.server.network.ServerGamePacketListenerImpl
+import org.bukkit.entity.Display
+import org.bukkit.entity.Player
+import org.bukkit.entity.TextDisplay
+import java.util.*
+
+class NPC(private val plugin: FireworkWarsLobbyPlugin, val data: NPCData) {
+    private val location = data.location.toBukkit()
+    private val world = location.world
+
+    private val nmsWorld = NMSUtil.toNMSWorld(world)
+    private val handle: ServerPlayer
+    val id: Int
+
+    private val nameTag: TextDisplay
+
+    init {
+        this.handle = createServerPlayer()
+        this.id = handle.id
+        this.nameTag = createNameTag()
+    }
+
+    private fun createServerPlayer(): ServerPlayer {
+        val profile: CraftPlayerProfile = plugin.server.createProfile(UUID.randomUUID()) as CraftPlayerProfile
+        profile.properties.add(
+            ProfileProperty("textures", data.skin.value, data.skin.signature))
+
+        val npc = ServerPlayer(nmsWorld.server!!, nmsWorld as ServerLevel, profile.gameProfile, ClientInformation.createDefault())
+        val entityData: SynchedEntityData = npc.entityData
+
+        val bitmask = (0x01 or 0x04 or 0x08 or 0x10 or 0x20 or 0x40).toByte()
+        entityData.set(EntityDataAccessor(17, EntityDataSerializers.BYTE), bitmask)
+
+        npc.connection = ServerGamePacketListenerImpl(
+            npc.server, EmptyConnection(null), npc, CommonListenerCookie.createInitial(npc.getGameProfile(), false))
+
+        return npc
+    }
+
+    private fun createNameTag(): TextDisplay {
+        val display = world.spawn(location, TextDisplay::class.java)
+
+        display.text(plugin.miniMessage.deserialize(data.name))
+        display.alignment = TextDisplay.TextAlignment.CENTER
+        display.billboard = Display.Billboard.VERTICAL
+        display.transformation.translation.let {
+            it.x = 0.0F
+            it.y = 0.2F
+            it.z = 0.0F
+        }
+
+        handle.bukkitEntity.addPassenger(display)
+        return display
+    }
+
+    fun sendInitPackets(player: Player) {
+        val connection: ServerGamePacketListenerImpl = NMSUtil.toNMSEntity<ServerPlayer>(player).connection
+
+        connection.send(ClientboundPlayerInfoUpdatePacket(Action.ADD_PLAYER, handle))
+        connection.send(ClientboundAddEntityPacket(handle, 0, BlockPos(location.blockX, location.blockY, location.blockZ)))
+        connection.send(ClientboundSetEntityDataPacket(id, handle.entityData.packAll()!!))
+
+        plugin.runTaskLater({
+            connection.send(ClientboundPlayerInfoRemovePacket(listOf(handle.uuid)))
+        }, 20L)
+    }
+
+    fun runLookAtTask() {
+        world.players.forEach {
+            val player = NMSUtil.toNMSEntity<ServerPlayer>(it)
+
+            handle.lookAt(EntityAnchorArgument.Anchor.EYES, player, EntityAnchorArgument.Anchor.EYES)
+            updateRotationPackets(it, handle.yRot, handle.xRot)
+
+            handle.yRot = 0.0F
+            handle.xRot = 0.0F
+        }
+    }
+
+    fun updateRotationPackets(player: Player, yaw: Float, pitch: Float) {
+        val connection: ServerGamePacketListenerImpl = NMSUtil.toNMSEntity<ServerPlayer>(player).connection
+
+        connection.send(ClientboundRotateHeadPacket(handle, yaw.toInt().toByte()))
+        connection.send(ClientboundMoveEntityPacket.Rot(
+            id,
+            yaw.toInt().toByte(),
+            pitch.toInt().toByte(),
+            handle.onGround()
+        ))
+    }
+}
