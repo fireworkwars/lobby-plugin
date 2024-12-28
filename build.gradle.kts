4421016f1fcb73a84a275d81024999468eb9c5ee
rolyPolyVole <yezhanting@gmail.com>
2024-12-28 12:42:57 +0000

    Fix shadow issue, add FWW core dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("jvm") version "2.1.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
+    id("io.papermc.paperweight.userdev") version "1.7.3"
 }
 
 group = "foundation.esoteric"
 version = "1.0-SNAPSHOT"
+
+val targetJavaVersion = 21
+
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
 
 repositories {
     mavenCentral()
@@ -14,27 +23,50 @@ repositories {
     maven("https://oss.sonatype.org/content/groups/public/") {
         name = "sonatype"
     }
+    mavenLocal()
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
+    compileOnly("foundation.esoteric:firework-wars-core-plugin:1.0-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.3-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("dev.triumphteam:triumph-gui:3.1.11")
 }
 
-val targetJavaVersion = 21
-kotlin {
-    jvmToolchain(targetJavaVersion)
-}
+tasks {
+    shadowJar {
+        minimize {
+            exclude(dependency("org.jetbrains.kotlin:kotlin-reflect"))
+        }
 
-tasks.build {
-    dependsOn("shadowJar")
-}
+        relocate("dev.triumphteam.gui", "foundation.esoteric.fireworkwarslobby.gui")
+    }
 
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+    build {
+        dependsOn(shadowJar)
+    }
+
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(21)
+    }
+
+    compileKotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
     }
 }
