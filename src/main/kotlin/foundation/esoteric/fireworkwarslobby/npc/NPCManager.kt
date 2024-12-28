142c56d2b2cc201d1784aec242149eec43a185ee
rolyPolyVole <yezhanting@gmail.com>
2024-12-28 12:48:50 +0000

    Add NPC manager

---
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/npc/NPCManager.kt
@@ -0,0 +1,26 @@
+package foundation.esoteric.fireworkwarslobby.npc
+
+import foundation.esoteric.fireworkwarslobby.FireworkWarsLobbyPlugin
+
+class NPCManager(private val plugin: FireworkWarsLobbyPlugin) {
+    val npcs = mutableMapOf<Int, NPC>()
+    val npcList = mutableListOf<NPC>()
+
+    fun spawnNPCs() {
+        plugin.configManager.lobbyConfig.npcs.forEach {
+            val npc = NPC(plugin, it)
+            plugin.runTaskTimer(npc::runLookAtTask, 0L, 1L)
+
+            registerNPC(npc)
+        }
+    }
+
+    fun getNPC(id: Int): NPC {
+        return npcs[id] ?: throw IllegalArgumentException("NPC with id $id not found")
+    }
+
+    private fun registerNPC(npc: NPC) {
+        npcs[npc.id] = npc
+        npcList.add(npc)
+    }
+}
