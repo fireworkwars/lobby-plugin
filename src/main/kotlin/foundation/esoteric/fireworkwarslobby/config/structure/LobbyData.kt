e8f3f4576cce4ae07a4e8291240bb0d067b26d9b
rolyPolyVole <yezhanting@gmail.com>
2024-12-30 00:26:45 +0100

    Add `randomFireworkLocations()` helper method

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/config/structure/LobbyData.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/config/structure/LobbyData.kt
@@ -1,14 +1,24 @@
 package foundation.esoteric.fireworkwarslobby.config.structure
 
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.World
 
 data class LobbyData (
     val worldName: String,
     val spawnLocation: EntityLocation,
+    val fireworkLocations: List<EntityLocation>,
     val npcs: List<NPCData>
 ) {
     fun getWorld(): World {
         return Bukkit.getWorld(worldName)!!
+    }
+
+    fun randomFireworkLocations(): List<Location> {
+        val fireworkLocs = fireworkLocations.map { it.toBukkit() }
+        val spawnLoc = spawnLocation.toBukkit()
+
+        val randomFireworks = fireworkLocs.shuffled().take(3)
+        return randomFireworks + spawnLoc
     }
 }
