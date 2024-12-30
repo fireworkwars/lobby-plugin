2e3e0c8c54392655b7d33b042e24ec30d7d1ef1c
rolyPolyVole <yezhanting@gmail.com>
2024-12-31 00:25:49 +0100

    Add gui lib ItemBuilder extension methods

---
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
@@ -0,0 +1,15 @@
+package foundation.esoteric.fireworkwarslobby.util
+
+import dev.triumphteam.gui.builder.item.ItemBuilder
+import foundation.esoteric.fireworkwarscore.language.LanguageManager
+import foundation.esoteric.fireworkwarscore.language.Message
+
+fun ItemBuilder.name(message: Message, vararg args: Any?): ItemBuilder {
+    return this.name(
+        LanguageManager.globalInstance!!.getDefaultMessage(message, *args))
+}
+
+fun ItemBuilder.lore(message: Message, vararg args: Any?): ItemBuilder {
+    return this.lore(
+        LanguageManager.globalInstance!!.getDefaultMessage(message, *args))
+}
