b5ad0d1fa64e438338f6d6cc37bf7d7d1512a2cf
rolyPolyVole <yezhanting@gmail.com>
2025-01-05 13:21:27 +0100

    Add `String#matchLength(String)` extension function to strings

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
@@ -3,6 +3,7 @@ package foundation.esoteric.fireworkwarslobby.util
 import dev.triumphteam.gui.builder.item.ItemBuilder
 import foundation.esoteric.fireworkwarscore.language.LanguageManager
 import foundation.esoteric.fireworkwarscore.language.Message
+import kotlin.math.max
 
 fun ItemBuilder.name(message: Message, vararg args: Any?): ItemBuilder {
     return this.name(
@@ -12,4 +13,9 @@ fun ItemBuilder.name(message: Message, vararg args: Any?): ItemBuilder {
 fun ItemBuilder.lore(message: Message, vararg args: Any?): ItemBuilder {
     return this.lore(
         LanguageManager.globalInstance.getDefaultMessage(message, *args))
+}
+
+fun String.matchLength(other: String): String {
+    val difference = max(0, other.length - length)
+    return this + " ".repeat(difference)
 }
