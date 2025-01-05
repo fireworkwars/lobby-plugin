d6576abb25b9513306d41d71cb6c6fb8f5030b58
rolyPolyVole <yezhanting@gmail.com>
2025-01-05 16:19:23 +0100

    Remove unused extension utils

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
@@ -1,19 +1,6 @@
 package foundation.esoteric.fireworkwarslobby.util
 
-import dev.triumphteam.gui.builder.item.ItemBuilder
-import foundation.esoteric.fireworkwarscore.language.LanguageManager
-import foundation.esoteric.fireworkwarscore.language.Message
 import kotlin.math.max
-
-fun ItemBuilder.name(message: Message, vararg args: Any?): ItemBuilder {
-    return this.name(
-        LanguageManager.globalInstance.getDefaultMessage(message, *args))
-}
-
-fun ItemBuilder.lore(message: Message, vararg args: Any?): ItemBuilder {
-    return this.lore(
-        LanguageManager.globalInstance.getDefaultMessage(message, *args))
-}
 
 fun String.matchLength(other: String): String {
     val difference = max(0, other.length - length)
