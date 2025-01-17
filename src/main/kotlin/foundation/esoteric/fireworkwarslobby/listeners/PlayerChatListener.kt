ad1fe4965f3d46c1854d12d211db1009f7b39503
rolyPolyVole <yezhanting@gmail.com>
2025-01-17 18:28:50 +0000

    Fix chat listener not removing the <playerName> before the message

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/listeners/PlayerChatListener.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/listeners/PlayerChatListener.kt
@@ -2,8 +2,11 @@ package foundation.esoteric.fireworkwarslobby.listeners
 
 import foundation.esoteric.fireworkwarscore.interfaces.Event
 import foundation.esoteric.fireworkwarslobby.FireworkWarsLobbyPlugin
+import io.papermc.paper.chat.ChatRenderer
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 
 class PlayerChatListener(private val plugin: FireworkWarsLobbyPlugin) : Event {
@@ -17,7 +20,12 @@ class PlayerChatListener(private val plugin: FireworkWarsLobbyPlugin) : Event {
             return
         }
 
-        val profile = plugin.playerDataManager.getPlayerProfile(event.player)
-        event.message(profile.formattedName().append(Component.text(":")).appendSpace().append(event.message()))
+        event.renderer(ChatRenderer.viewerUnaware { _, _, message -> message })
+        event.message(this.formatMessage(event.player, event.originalMessage()))
+    }
+
+    private fun formatMessage(player: Player, message: Component): Component {
+        val profile = plugin.playerDataManager.getPlayerProfile(player)
+        return profile.formattedName().append(Component.text(": ").color(NamedTextColor.WHITE)).append(message)
     }
 }
