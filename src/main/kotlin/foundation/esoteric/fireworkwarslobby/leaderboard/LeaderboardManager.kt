9e3cecf317c6a3dc5e68dfeb3c89e6a7d1a0ff29
rolyPolyVole <yezhanting@gmail.com>
2025-01-26 22:06:39 +0000

    Refactor code + add interaction id to leaderboard display map

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/leaderboard/LeaderboardManager.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/leaderboard/LeaderboardManager.kt
@@ -1,7 +1,5 @@
 package foundation.esoteric.fireworkwarslobby.leaderboard
 
-import foundation.esoteric.fireworkwarscore.language.Message
-import foundation.esoteric.fireworkwarscore.util.getMessage
 import foundation.esoteric.fireworkwarslobby.FireworkWarsLobbyPlugin
 import org.bukkit.entity.Player
 import java.util.*
@@ -9,40 +7,30 @@ import java.util.*
 class LeaderboardManager(private val plugin: FireworkWarsLobbyPlugin) {
     private val config = plugin.configManager.lobbyConfig.leaderboards
 
-    private val winLeaderboards = mutableMapOf<UUID, LeaderboardDisplay>()
-    private val killLeaderboards = mutableMapOf<UUID, LeaderboardDisplay>()
+    private val leaderboards = mutableMapOf<UUID, List<LeaderboardDisplay>>()
+    val leaderboardMap = mutableMapOf<Int, LeaderboardDisplay>()
 
     init {
         plugin.runTaskTimer({
             this.updateAll()
-        }, 0L, 20L * 10)
+        }, 0L, 20L * 30)
     }
 
     fun createOrUpdateLeaderboard(player: Player) {
-        winLeaderboards.computeIfAbsent(player.uniqueId) {
-            LeaderboardDisplay(plugin, config.allTimeWinsLocation, player).apply {
-                this.title = player.getMessage(Message.LEADERBOARD_TITLE)
-                this.subtitle = player.getMessage(Message.LEADERBOARD_TYPE_WINS)
-                this.valueFunction = { it.stats.wins }
+        leaderboards.computeIfAbsent(player.uniqueId) {
+            config.map {
+                LeaderboardDisplay(it, plugin, player).apply {
+                    leaderboardMap[id] = this
+                }
             }
-        }.let(LeaderboardDisplay::updateAndSendPackets)
-
-        killLeaderboards.computeIfAbsent(player.uniqueId) {
-            LeaderboardDisplay(plugin, config.allTimeKillsLocation, player).apply {
-                this.title = player.getMessage(Message.LEADERBOARD_TITLE)
-                this.subtitle = player.getMessage(Message.LEADERBOARD_TYPE_KILLS)
-                this.valueFunction = { it.stats.kills }
-            }
-        }.let(LeaderboardDisplay::updateAndSendPackets)
+        }.forEach(LeaderboardDisplay::updateAndSendPackets)
     }
-
+    
     private fun updateAll() {
-        winLeaderboards.values.forEach(LeaderboardDisplay::updateAndSendPackets)
-        killLeaderboards.values.forEach(LeaderboardDisplay::updateAndSendPackets)
+        leaderboards.values.flatten().forEach(LeaderboardDisplay::updateAndSendPackets)
     }
 
     fun deleteLeaderboard(player: Player) {
-        winLeaderboards.remove(player.uniqueId)
-        killLeaderboards.remove(player.uniqueId)
+        leaderboards.remove(player.uniqueId)
     }
 }
