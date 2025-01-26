cf9823ce11c175ddae9ad621659c9a6ddd1b3931
rolyPolyVole <yezhanting@gmail.com>
2025-01-26 22:01:34 +0000

    Update core dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.esotericfoundation:firework-wars-core-plugin:0.4.2")
+    compileOnly("com.github.esotericfoundation:firework-wars-core-plugin:0.5.0")
 
     paperweight.paperDevBundle("$paperApiVersion-R0.1-SNAPSHOT")
 
