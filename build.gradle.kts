81ec67871bbe10334f002e35ea7bce444fad6491
rolyPolyVole <yezhanting@gmail.com>
2025-01-28 21:57:02 +0000

    Update core dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,12 @@ plugins {
     kotlin("jvm") version "2.1.0"
     id("com.gradleup.shadow") version "8.3.5"
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.2.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
 }
+
+val buildToMain = false
+val buildToCore = false
 
 group = "foundation.esoteric"
 version = "1.0-SNAPSHOT"
@@ -32,11 +36,20 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.esotericfoundation:firework-wars-core-plugin:0.5.1")
+    compileOnly("com.github.esotericfoundation:firework-wars-core-plugin:0.5.3")
 
     paperweight.paperDevBundle("$paperApiVersion-R0.1-SNAPSHOT")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks.withType(xyz.jpenilla.runtask.task.AbstractRun::class) {
+    javaLauncher = javaToolchains.launcherFor {
+        vendor = JvmVendorSpec.JETBRAINS
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+
+    jvmArgs("-XX:+AllowEnhancedClassRedefinition")
 }
 
 tasks {
@@ -45,7 +58,11 @@ tasks {
             exclude(dependency("org.jetbrains.kotlin:kotlin-reflect"))
         }
 
-        destinationDirectory.set(file("../firework-wars-plugin/run/plugins"))
+        if (buildToMain) {
+            destinationDirectory.set(file("../firework-wars-plugin/run/plugins"))
+        } else if (buildToCore) {
+            destinationDirectory.set(file("../firework-wars-core-plugin/run/plugins"))
+        }
     }
 
     build {
