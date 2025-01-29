1d2ade75738760c3baad3e3968103e623d846ba9
rolyPolyVole <yezhanting@gmail.com>
2025-01-29 19:40:52 +0000

    Add `Component.length` extension function

--- src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarslobby/util/ExtnUtil.kt
@@ -1,1 +1,13 @@
 package foundation.esoteric.fireworkwarslobby.util
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.minimessage.MiniMessage
+
+fun Component.length(): Int {
+    val decorationsMap = this.decorations().map { it.key to TextDecoration.State.NOT_SET }.toMap()
+    val withoutDecorations = this.compact().decorations(decorationsMap)
+    val withoutEvents = withoutDecorations.clickEvent(null).hoverEvent(null)
+
+    return MiniMessage.miniMessage().serialize(withoutEvents).length
+}
