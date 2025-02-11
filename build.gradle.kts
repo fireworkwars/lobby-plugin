70ed620e910c8233143c81c2e23f0e51902f0984
rolyPolyVole <yezhanting@gmail.com>
2025-02-11 19:38:21 +0000

    Update core dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ version = "1.1.2"
 description = "Lobby features for a Firework Wars server."
 
 val paperApiVersion = "1.21.4"
-val corePluginDependencyVersion = "1.4.1"
+val corePluginDependencyVersion = "1.4.2"
 val targetJavaVersion = 21
 
 // authors
