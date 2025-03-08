8df9901a48d953569cfc750142b79a0f8932d0b9
rolyPolyVole <yezhanting@gmail.com>
2025-03-08 19:38:44 +0000

    Update core version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ version = "1.4.0"
 description = "Lobby features for a Firework Wars server."
 
 val paperApiVersion = "1.21.4"
-val corePluginDependencyVersion = "1.12.0"
+val corePluginDependencyVersion = "1.14.2"
 val targetJavaVersion = 21
 
 val mainClassPath = "$group.lobby.FireworkWarsLobbyPlugin"
