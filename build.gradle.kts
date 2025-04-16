88e75a90cccbedec171368865babd54f0799448f
rolyPolyVole <yezhanting@gmail.com>
2025-04-16 13:24:38 +0100

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val buildToMain = false
 val buildToCore = false
 
 group = "xyz.fireworkwars"
-version = "1.7.3"
+version = "1.8.0"
 description = "Lobby features for a Firework Wars server."
 
 val paperApiVersion = "1.21.4"
