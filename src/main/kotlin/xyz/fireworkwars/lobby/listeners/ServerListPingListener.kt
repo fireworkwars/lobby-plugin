dc4b5a342bd2e5129f8eb713872c78081f583928
rolyPolyVole <yezhanting@gmail.com>
2025-06-02 20:00:36 +0700

    Display random configured MOTD

--- src/main/kotlin/xyz/fireworkwars/lobby/listeners/ServerListPingListener.kt
+++ src/main/kotlin/xyz/fireworkwars/lobby/listeners/ServerListPingListener.kt
@@ -3,11 +3,13 @@ package xyz.fireworkwars.lobby.listeners
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent
 import org.bukkit.event.EventHandler
 import xyz.fireworkwars.core.interfaces.Event
+import xyz.fireworkwars.core.language.Message
 import xyz.fireworkwars.lobby.FireworkWarsLobbyPlugin
 import kotlin.math.roundToInt
 import kotlin.math.sin
 
 class ServerListPingListener(private val plugin: FireworkWarsLobbyPlugin) : Event {
+    private val config = plugin.configManager.lobbyConfig
     private val server = plugin.server
 
     override fun register() {
@@ -16,6 +18,7 @@ class ServerListPingListener(private val plugin: FireworkWarsLobbyPlugin) : Even
 
     @EventHandler
     fun onServerListPing(event: PaperServerListPingEvent) {
-        event.numPlayers = 8 + sin(0.001 * server.currentTick).roundToInt() + server.onlinePlayers.size
+        event.numPlayers = config.fakePlayers + sin(0.001 * server.currentTick).roundToInt() + server.onlinePlayers.size
+        event.motd(plugin.languageManager.getDefaultMessage(Message.valueOf(config.motdMessages.random())))
     }
 }
